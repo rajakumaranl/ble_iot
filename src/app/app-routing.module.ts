eecab40c4dffbacf973254525119407521b88dbf
Rajakumaran <rajakumaran.lakshmanan@umuc.edu>
2020-12-11 09:44:45 +0530

    separated server

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { BLEDashboardComponent } from './bledashboard/bledashboard.component';
 
 const routes: Routes = [
                           {
-                            path: '/view-devices',
+                            path: 'view-devices',
                             component: BLEDashboardComponent,
                             data: {
                                 title: 'View Devices in office'
